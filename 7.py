a126348f5aed12ca5f93fdacfb999342299995d2
perizatzhengiskyzy <pzeniskyzy@gmail.com>
2025-01-26 18:43:49 +0500

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,13 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+  thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
---
+++ 8.py
@@ -0,0 +1,19 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
