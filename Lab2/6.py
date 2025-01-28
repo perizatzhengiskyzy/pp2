54bb5be94cbfe492dacccee8314263386f98606f
perizatzhengiskyzy <pzeniskyzy@gmail.com>
2025-01-28 13:24:12 +0500

    lab3

---
+++ Lab2/6.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
---
+++ Lab2/9.py
@@ -0,0 +1,10 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
